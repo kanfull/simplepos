25ce50b010e8e40108cc9e221d57a378831bfa1f
Kan Kundhikanjana <kanfull@gmail.com>
2020-07-22 23:17:32 +0700

    Update schema.sql
    Update schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,43 +1,58 @@
-CREATE TABLE `order` (
-  `order_id` int NOT NULL AUTO_INCREMENT,
-  `order_description` varchar(200) DEFAULT NULL,
+CREATE TABLE `employee` (
+  `employee_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(33) DEFAULT NULL,
+  `token_expire_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `order_item` (
-  `order_item_id` int NOT NULL AUTO_INCREMENT,
-  `order_id` int DEFAULT NULL,
-  `product_id` int DEFAULT NULL,
-  `promotion_id` int DEFAULT NULL,
-  `sale_price` decimal(10,2) DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`order_list_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `price` (
   `price_id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int DEFAULT NULL,
   `price_value` decimal(10,2) DEFAULT NULL,
+  `valid` tinyint DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`price_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `product` (
   `product_id` int NOT NULL AUTO_INCREMENT,
   `product_name` varchar(45) DEFAULT NULL,
-  `price_id` int DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `promotion` (
   `promotion_id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int DEFAULT NULL,
   `promotion_price` decimal(10,2) DEFAULT NULL,
+  `valid` tinyint DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`promotion_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `sale` (
+  `sale_id` int NOT NULL AUTO_INCREMENT,
+  `sale_description` varchar(200) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`sale_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `sale_item` (
+  `sale_item_id` int NOT NULL AUTO_INCREMENT,
+  `sale_id` int DEFAULT NULL,
+  `product_id` int DEFAULT NULL,
+  `price_id` int DEFAULT NULL,
+  `promotion_id` int DEFAULT NULL,
+  `sale_price` decimal(10,2) DEFAULT NULL,
+  `unit` int DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`sale_item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
